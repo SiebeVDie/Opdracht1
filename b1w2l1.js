f4ba3f6be6691ee3218e0b7c4ea6f9bf47d21f5f
Siebe van Die <99059545@mydavinci.nl>
2019-09-13 11:14:31 +0200

    even wat mooier gemaakt

--- b1w2l1.js
+++ b1w2l1.js
@@ -1,10 +1,12 @@
 var small = prompt('hoeveel small pizzas wilt u. €8 per stuk');
 var medium = prompt('hoeveel medium pizzas wilt u. €11 per stuk');
 var large = prompt('hoeveel large pizzas wilt u. €13 per stuk');
+
 var prijsSmall = 8
 var prijsMedium = 11
 var prijsLarge = 13
 var prijs = prijsSmall * small + prijsMedium * medium + prijsLarge * large
+
 var LossePrijsSmall = small * prijsSmall
 var LossePrijsMedium = medium * prijsMedium
 var LossePrijslarge = large * prijsLarge 
@@ -15,11 +17,15 @@ document.write('<br>')
 document.write('€' + LossePrijsSmall);
 document.write('<br>')
 document.write('<br>')
+
+
 document.write('Hoeveelheid medium:  ' + medium);
 document.write('<br>')
 document.write('€' + LossePrijsMedium)
 document.write('<br>')
 document.write('<br>')
+
+
 document.write('Hoeveelheid large:  ' + large);
 document.write('<br>')
 document.write('€' + LossePrijslarge)
